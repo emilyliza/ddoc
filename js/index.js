1281d14d3d94320e03c12ae4f47aeaf7cbf4cfb1
emilyliza <emilyliza@gmail.com>
2018-09-27 16:42:36 -0700

    copy changes

--- js/index.js
+++ js/index.js
@@ -7,6 +7,6 @@ $('.panel-collapse').on('shown.bs.collapse', function(e) {
         scrollTop: $panel.offset().top
       }, 500);
 });
-$(".panel-collapse").sortable({
+$(".panel-group").sortable({
     handle: ".panel-heading"
 });
