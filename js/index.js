9304006766d639d301517871e5f356e1c560e848
emilyliza <emilyliza@gmail.com>
2018-09-27 12:42:03 -0700

    copy changes

--- js/index.js
+++ js/index.js
@@ -7,14 +7,4 @@ $('.panel-collapse').on('shown.bs.collapse', function(e) {
         scrollTop: $panel.offset().top
       }, 500);
 });
-$('.panel').sortable({
-        axis: "y",
-        handle: "h4",
-        stop: function( event, ui ) {
-          // IE doesn't register the blur when sorting
-          // so trigger focusout handlers to remove .ui-state-focus
-          ui.item.children( "h3" ).triggerHandler( "focusout" );
- 
-          // // Refresh accordion to handle new order
-          // $( this ).accordion( "refresh" );
-        }
+$('.panel').sortable():
