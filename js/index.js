2507d3adcbccd339b7aeaf355e999bf0c1ee885e
emilyliza <emilyliza@gmail.com>
2018-09-27 11:33:12 -0700

    bootstrap version

--- js/index.js
+++ js/index.js
@@ -1,15 +1,9 @@
-$('.toggle').click(function(e) {
-  	e.preventDefault();
-  
-    var $this = $(this);
-  
-    if ($this.next().hasClass('show')) {
-        $this.next().removeClass('show');
-        $this.next().slideUp(350);
-    } else {
-        $this.parent().parent().find('li .inner').removeClass('show');
-        $this.parent().parent().find('li .inner').slideUp(350);
-        $this.next().toggleClass('show');
-        $this.next().slideToggle(350);
-    }
+$('.panel-collapse').on('shown.bs.collapse', function(e) {
+    e.preventDefault();  
+    e.stopPropagation();
+
+      var $panel = $(this).closest('.panel');
+      $('html,body').animate({
+        scrollTop: $panel.offset().top
+      }, 500);
 });
