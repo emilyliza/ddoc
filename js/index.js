c843ec478808c1388438d691affafdcb8e1ab2ae
emilyliza <emilyliza@gmail.com>
2018-09-28 15:28:41 -0700

    test accordion

--- js/index.js
+++ js/index.js
@@ -32,11 +32,22 @@ data.forEach(function(item) {
 var count = 0;
 var html = $.map(cat, function(body, category) {
     count += 1;
-    return '<div class="panel-heading" role="tab" id="heading-"' + count + '><h4 class="panel-title"><a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">'
+    return '<div class="bs-example" data-example-id="collapse-accordion">
+            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                <div class="panel panel-default"><div class="panel-heading" role="tab" id="heading-' + count + '"><h4 class="panel-title"><a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">'
     + category + "</a></h4></div>" +
-        "<div>" + $.map(body, function(item, i) {
-            return "<h5>" + item.solution + "</h5>" +
-                "<div>" + item.description + "</div>"
+        '<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne" aria-expanded="false">
+                        <div class="panel-body">
+                            <div class="panel-group" id="accordion1" role="tablist" aria-multiselectable="true">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading" role="tab" id="headingOne1">
+                                        <h4 class="panel-title">
+                                            <a role="button" data-toggle="collapse" data-parent="#accordion1" href="#collapseOne1" aria-expanded="false" aria-controls="collapseOne1" class="collapsed"> ' + $.map(body, function(item, i) {
+            return item.solution + "</a>
+                                        </h4>
+                                    </div>" +
+                '<div id="collapse-' + count +'" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne1" aria-expanded="false">
+                                        <div class="panel-body"> ' + item.description + '</div></div></div></div></div></div>'
         }).join("") + "</div>"
 }).join("")
 
