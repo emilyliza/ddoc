03cbfe7a41e9c491687856c3f6e27f0474d3b6b0
emilyliza <emilyliza@gmail.com>
2018-09-28 15:53:47 -0700

    another test

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
-var data = [
-{ sector: "Land Use",
-  solution: "solution1",
+var solutions = [
+{ sector: "Land Use" {solution: "solution1"}
+  ,
   description: "solution description hggfghf"
 },
 { sector: "Land Use",
@@ -21,6 +21,55 @@ var data = [
   description: "solution description gjgyhjbjh"
 }
 ];
+
+var json = {
+        residus: [
+            {
+              name:'1 level XXX ', childs: [
+                {name:'element 1.1', childs: null},
+                {name:'element 1.2 jh', childs: null}
+              ]
+            },
+            {
+              name:'1 level  ', childs: [
+                {name:'element 1.1', childs: null},
+                {name:'element 1.2 jh', childs: [
+                      {
+                          name:'element 2.1.1', childs:null
+                          
+                        },
+                        {
+                          name:'element 2.1.2', childs:null
+                          
+                        }
+                      ]}
+              ]
+            },
+            {
+              name:'2 level 1.. ', childs: [
+                {
+                  name:'element 2.1', url: "json.js"
+                }
+              ]
+            },
+            {
+                name:'2 level 1.. ',   url: "json1.js" 
+              }
+          ]
+      };
+
+var collapseTemplate = Object.create(CollapseTemplate);
+    var params = {
+        templateSelector: 'div.templateContainer > div.template',
+        parentId: 'accordion'
+     };
+    
+    jQuery(document).ready( function() {
+
+      collapseTemplate.init(params);
+      collapseTemplate.load(json);
+        
+  });
 
 $('.panel-collapse').on('shown.bs.collapse', function(e) {
     e.preventDefault();  
