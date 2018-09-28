ccb38d9a2cb37d46a7a780aa5c3115af9ac87cfb
emilyliza <emilyliza@gmail.com>
2018-09-28 10:03:38 -0700

    test accordion

--- js/index.js
+++ js/index.js
@@ -33,10 +33,8 @@ var html = $.map(cat, function(body, category) {
     return "<h3>" + category + "</h3>" +
         "<div>" + $.map(body, function(item, i) {
             console.log(i, item)
-            return "<h3>" + item.solution + "</h3>" +
-                "<div>" + $.map(item, function(line, key) {
-                    return "<h3>"+key+"</h3><div>"+line+"</div>"
-                }).join("") + "</div>"
+            return "<h5>" + item[i].solution + "</h5>" +
+                "<div>" + item[i].description + "</div>"
         }).join("") + "</div>"
 }).join("")
 $(function(){
