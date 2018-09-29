b0588056cf562697972e7fbe463f6d802e24a92a
emilyliza <emilyliza@gmail.com>
2018-09-29 16:55:43 -0700

    another test

--- js/script.js
+++ js/script.js
@@ -1,4 +1,59 @@
 // Code goes here
+ var collapseTemplate = Object.create(CollapseTemplate);
+    var params = {
+        templateSelector: 'div.templateContainer > div.template',
+        parentId: 'accordion'
+     };
+    
+    jQuery(document).ready( function() {
+
+      collapseTemplate.init(params);
+      collapseTemplate.load(json);
+      
+      $("button").on("click", function(){
+        json.residus[0].name="Xavi Abarca";
+        collapseTemplate.load(json);
+      });
+        
+  });
+        
+
+ var json = {
+        residus: [
+            {
+              name:'1 level XXX ', childs: [
+                {name:'element 1.1', childs: null},
+                {name:'element 1.2 jh', childs: null}
+              ]
+            },
+            {
+              name:'1 level  ', childs: [
+                {name:'element 1.1', childs: null},
+                {name:'element 1.2 jh', childs: [
+                      {
+                          name:'element 2.1.1', childs:null
+                          
+                        },
+                        {
+                          name:'element 2.1.2', childs:null
+                          
+                        }
+                      ]}
+              ]
+            },
+            {
+              name:'2 level 1.. ', childs: [
+                {
+                  name:'element 2.1', childs:null
+                }
+              ]
+            },
+            {
+                name:'2 level 1.. ',  childs:null
+              }
+          ]
+      };
+
 
 var CollapseTemplate = {
 	params : {
