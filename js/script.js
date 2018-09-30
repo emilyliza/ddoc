17283044e3ed1e7ef01495725bb11eeb7e8c2302
emilyliza <emilyliza@gmail.com>
2018-09-29 17:28:55 -0700

    another test

--- js/script.js
+++ js/script.js
@@ -40,13 +40,11 @@ var CollapseTemplate = {
 	},
 
 	init : function(_params) {
-		//sobreescibimos los params
 		this.params = Utils.extend({}, this.params, _params);
 	},
 	
-	//cargamos el JSON inicial
+	
 	load : function(json) {
-		// borramos el panel principal
 		$("#" + this.params.parentId).empty();
 		var _this = this;
 		$.each(json.residus, function(i, val) {
@@ -56,26 +54,18 @@ var CollapseTemplate = {
 
 	draw : function(name, childs, panel, url) {
 		console.log(childs);
-		//pillamos un numero que se aumenta para los ids
 		var numId = Global.getNextNumCollapseElement();
-		//pillamos el template
 		var template = $(this.params.templateSelector);
-		//lo cloneamos
 		var $newPanel = template.clone();
 		var dataParentId = this.params.parentId;
-		//si el panel no esta definido el padre es el container definido en los params
 		if (panel !== undefined) {
 			dataParentId = $(panel).find(".panel-collapse").attr("id");
 		}
 		
-		//añadimos una id al panel
 		$($newPanel).attr("id", "panel" + numId);
-		//pintamos elemento header
 		this.drawHeader(name,  $newPanel, numId, dataParentId);
-		//manejamos los hijos
 		this.drawChildNodes(childs,  $newPanel, numId, url);
 		
-		//si es el primer nivel añadimos al padre sino al panel creado
 		if (panel === undefined) {
 			$("#" + this.params.parentId).append($newPanel.show());
 		} else {
@@ -97,15 +87,15 @@ var CollapseTemplate = {
 		}
 	},
 	
-	//pinta los hijos apartir de un array
+
 	drawChildNodesArray : function(childs,  $newPanel, numId) {
 		var _this = this;
-		// cargamos los hijos del array
+
 		$.each(childs, function(i, val) {
 			if (val.childs !== null) {
 				_this.draw(val.name, val.childs, $newPanel, val.url);
-			//si se cargan los hijos por ajax
 			} else if (val.childs === null && val.url !== undefined && val.url !== null) {
+				$newPanel.find(".panel-title").append("<input type='checkbox'/>");
 				$newPanel.find(".panel-body").append("<ul class='list-group' id='list-group-"+numId+"'><li class='list-group-item'>loading</li></ul>");
 				$("#" + _this.params.parentId).on('click', 'a#link-'+numId ,function() {
 					var jqxhr = Utils.doAjax({}, val.url);
@@ -119,7 +109,6 @@ var CollapseTemplate = {
 								_this.draw(val.name, null, $("#panel"+numId), val.url);
 							}
 						});
-						//una vez cargado en la DOM quitamos el evento para no hacer más llamadas ajax
 						$("#" + _this.params.parentId).off('click', 'a#link-'+numId );
 					});
 				});
@@ -131,16 +120,15 @@ var CollapseTemplate = {
 
 	},
 	
-	//pinta los hijos apartir de una URL
-	//TODO codigo duplicado (drawChildNodesArray)
+	
 	drawChildNodesAjax : function(url, $newPanel, numId) {
 		var _this = this;
 		$newPanel.find(".panel-body").append("<ul class='list-group' id='list-group-"+numId+"'><li class='list-group-item'>loading</li></ul>");
-		//añadimos un cklick-event para que se carquen por ajax cuando se haga click en el link
+		
 		$("#" + _this.params.parentId).on('click', 'a#link-'+numId ,function() {
-			//una vez se ha hecho click se llama a la URL por AJAX
+			
 			var jqxhr = Utils.doAjax({}, url);
-			//manejamos la promesa
+			
 			jqxhr.done(function(dades) {
 				var panel = $("#" + _this.params.parentId).find("#collapse"+numId+" > div.panel-body");
 				$(panel).empty();
@@ -151,7 +139,6 @@ var CollapseTemplate = {
 						_this.draw(val.name, null, $("#panel"+numId), val.url);
 					}
 				});
-				//una vez cargado en la DOM quitamos el evento para no hacer más llamadas ajax
 				$("#" + _this.params.parentId).off('click', 'a#link-'+numId );
 			});
 		});
@@ -168,8 +155,6 @@ Global =	{
 };
 
 Utils = {
-	// extender un object con otro, sobrescribe los properties que se llaman
-	// igual
 	extend : function(dest) {
 		var sources = Array.prototype.slice.call(arguments, 1);
 		sources.forEach(function(source) {
@@ -180,7 +165,6 @@ Utils = {
 		return dest;
 	},
 
-	// hacer la llamada ajax con JSON
 	doAjax : function(params, _url) {
 		return $.ajax({
 			url : _url,
