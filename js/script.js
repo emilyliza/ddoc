9e5b3961b6c89c0a89cf894b23b3f6b5aedb7f81
emilyliza <emilyliza@gmail.com>
2018-10-02 16:03:09 -0700

    another test

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
 // Code goes here
- 
- $('.panel-collapse').on('shown.bs.collapse', function(e) {
-    e.preventDefault();  
-    e.stopPropagation();
-
-      var $panel = $(this).closest('.panel');
-      $('html,body').animate({
-        scrollTop: $panel.offset().top
-      }, 500);
-});
-$(".panel-group").sortable({
-    handle: ".panel-heading"
-});    
-
  var json = {
         solutions: [
             {
