18b3808e992a4edf7c7846dee391f4fbe121a0f0
emilyliza <emilyliza@gmail.com>
2018-10-03 06:32:20 -0700

    experimenting with sortable

--- js/index.js
+++ js/index.js
@@ -12,3 +12,6 @@ $('.panel-collapse').on('shown.bs.collapse', function(e) {
 $(".panel-group").sortable({
     handle: ".panel-heading"
 });
+$(".panel-default").sortable({
+    handle: ".panel-heading"
+});
